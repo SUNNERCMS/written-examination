d61c40bfb6c9e1c922f3311b5b6d0fe913168117
SUNNERCMS <309355484@qq.com>
2018-09-04 13:38:04 +0800

    Update 原生JS模拟apply()方法.js

--- 原生JS模拟apply()方法.js
+++ 原生JS模拟apply()方法.js
@@ -81,7 +81,7 @@ Function.prototype.applyone=function(target){
 	target[m]=this;
 	var arg=arguments[1];
 	if(arg.length===0){   //（2）没有参数传入时
-		return target.m();
+		return target[m]();
 	}
 	var str='target[m](';
 	for(var i=0,len=arg.length;i<len;i++){
@@ -89,8 +89,66 @@ Function.prototype.applyone=function(target){
 	}
 	var str1=str+')';
 	var result=eval(str1); //（3）指定方法存在返回值时，用变量将计算的结果进行缓存返回
-	delete target.m;
+	delete target[m];
 	return result;
 };
 jawil.sayHello.applyone(lulin,[24,32,34,54]);
 ---{name: "lulin", age: 24}
+//借用ES6语法的扩展运算符可以把数组转化为参数序列，简化上面的循环拼接的eval计算过程
+Function.prototype.applyone=function(target){
+	target=target||window;  //第一个参数没有或者为null时
+	var m=Symbol();	       //（4）当目标对象中已经存在了要指定的属性名或方法名
+	target[m]=this;
+	var arg=arguments[1];
+	if(arg.length===0){   //（2）没有参数传入时
+		return target[m]();
+	}
+	var result=target[m](...Array.from(arg)); //（3）指定方法存在返回值时，用变量将计算的结果进行缓存返回
+	delete target[m];
+	return result;
+};
+//call方法的原生Js实现
+Function.prototype.newCall=function(target){
+	target=target||window;  //第一个参数没有或者为null时
+	var m=Symbol();	       //（4）当目标对象中已经存在了要指定的属性名或方法名
+	target[m]=this;
+	var arg=Array.from(arguments).slice(1);  //call中的参数是一个一个的，不是数组
+	if(arg.length===0){   //（2）没有参数传入时
+		return target[m]();
+	}
+	var result=target[m](...arg); //利用扩展运算符把数组转换成参数序列
+	delete target[m];
+	return result;
+};
+var person = {
+  name: 'Abiel'
+}
+function sayHi(age,sex) {
+  console.log(this.name, age, sex);
+}
+sayHi.newCall (person,25,'男'); // Abiel 25 男
+//bind的模拟方法：主要是用到了apply方法，以及初始参数和实际参数的传参处理，另外兼容了绑定函数作为构造函数的情况
+var sun={
+	name:"aaa"
+}
+var rrr={
+	name:"ddd",
+	show:function(age){
+		console.log(this.name,age);
+	}
+}
+Function.prototype.newbind=function(target){
+	var fn=this;
+	var outargs=Array.prototype.slice.call(arguments,1);
+	var F=function(){};
+	F.prototype=this.prototype;
+	var bound=function(){
+		var innerargs=Array.prototype.slice.call(arguments);
+		var totalargs=outargs.concat(innerargs);
+		return fn.apply(this instanceof F?this:target,totalargs);
+	}
+	bound.prototype=new F();
+	return bound;
+}
+rrr.show.newbind(sun,23)();
+VM262:7 aaa 23

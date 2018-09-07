91ea192b523ca1251fb67ac448d6266ba7b29a92
SUNNERCMS <309355484@qq.com>
2018-09-07 22:15:08 +0800

    Update 动态规划--不相邻元素所能组成的最大值.js

--- 动态规划--不相邻元素所能组成的最大值.js
+++ 动态规划--不相邻元素所能组成的最大值.js
@@ -12,3 +12,20 @@ function dp(arr){
 }
 arr=[1,2,4,1,7,8,3];
 dp(arr);
+
+
+问题：从一组数中匹配出和为S的数，如能找到这样的一组数返回true，否则返回false
+//递归写法
+var arr=[3,34,4,12,5,2];
+function subset(arr,i,s){
+	if(s===0){
+		return true;
+	}else if(i===0){
+		return arr[0]===s;
+	}else if(arr[i]>s){
+		return subset(arr,i-1,s);
+	}else{
+		return subset(arr,i-1,s-arr[i])||subset(arr,i-1,s)?true:false;
+	}
+}
+subset(arr,arr.length,69);//false

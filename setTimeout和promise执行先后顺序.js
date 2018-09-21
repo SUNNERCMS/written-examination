84f9e8a3a0f289e31b8c2eae6d983d24b955fa6c
SUNNERCMS <309355484@qq.com>
2018-09-21 17:49:12 +0800

    Update setTimeout和promise执行先后顺序.js

--- setTimeout和promise执行先后顺序.js
+++ setTimeout和promise执行先后顺序.js
@@ -5,7 +5,7 @@ setTimeout(function(){
 new Promise(function(resolve){
 	console.log(2);
 	for(var i=0;i<10;i++){
-		i==9&&resolve();
+		i==9&&resolve();//&&的短路用法，只要当前面的值满足条件时才执行后面的
 	}
 	console.log(3);
 }).then(function(){

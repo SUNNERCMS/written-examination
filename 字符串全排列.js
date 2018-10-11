b257e8cb7338cf51f5482c7ca8b0938c60657d6f
SUNNERCMS <sunnercms@163.com>
2018-10-11 17:48:21 +0800

    Update 字符串全排列.js

--- 字符串全排列.js
+++ 字符串全排列.js
@@ -11,4 +11,6 @@ function anagrams(str){
 }
 anagrams('123');
 //代码拆分：
-anagrams(str.slice(0,i)+str.slice(i+1))//将除了下标为i的字母外，其余的在带入全排列函数进行全排列
+anagrams(str.slice(0,i)+str.slice(i+1))//将除了下标为i的字母外，其余的在带入全排列函数进行全排列，返回数组ARR
+.map(item=>letter+item)//将得到的全排列数组ARR中的元素和下标为i的字母进行合并，返回一个新数组
+acc.concat//字符串的长度可能不止为3，当长度过大时，递归的过程需要用acc进行存储合并。

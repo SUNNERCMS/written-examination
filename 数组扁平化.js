7b48a051a33933062a2e96f1c5bcd2291bac3204
SUNNERCMS <sunnercms@163.com>
2018-12-25 19:49:24 +0800

    Update 数组扁平化.js

--- 数组扁平化.js
+++ 数组扁平化.js
@@ -1,6 +1,8 @@
 利用generator函数和yield* 命令实现数组扁平化
 
 // yield*命令可以很方便的取出嵌套在数组中的所有成员
-function* iterTree(tree）{
-                    
+              function* iterTree(tree）{
+                     if(Array.isArray(tree)){
+                          for()
+                        }
                     }
